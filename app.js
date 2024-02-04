0dd9e5260b3921b7606bcd00f5822ca3b3a6038d
Ayush Kamble <aykkamble098@gmail.com>
2024-02-04 14:43:01 +0530

    added cors and conn to db

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 const express = require('express'); 
+const mysql = require('mysql');
+const cors = require('cors');
 
 const app = express(); 
 const PORT = 3000; 
+app.use(cors());
+cors({ credentials: true, origin: true })
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'QazWsxEdc@1092',
+    database: 'expense',
+});
+
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+    } else {
+        console.log('Connected to the database');
+    }
+});
 
  
 app.listen(PORT, (error) =>{ 
@@ -11,3 +31,10 @@ app.listen(PORT, (error) =>{
         console.log("Error occurred, server can't start", error); 
     } 
 ); 
+
+
+app.post('/CreateUser', (req, res) => {
+    console.log(req.body);
+    
+    res.json({requestBody: req.body});
+});
